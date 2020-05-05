65d1242252dad58f361487e88cd91f930baf4adc
yvonne110713231 <yvonne110713231@gmail.com>
2020-05-05 22:20:27 +0800

    add javascript

---
+++ homework/javascript/hw1.js
@@ -0,0 +1,4 @@
+sum=1;
+for(i=1;i<=10;i++){
+    sum*=i;}
+console.log("10!=",sum);
---
+++ homework/javascript/hw2.js
@@ -0,0 +1,8 @@
+for(i = 1; i < 10; i++)
+{
+  for(j = 1; j < 10; j++)
+  {
+    sum = i * j;
+    console.log(i + '*' + j + '=' + sum);
+  }
+}
